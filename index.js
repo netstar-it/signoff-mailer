f5919ea8786c16557316bf0f38dff8a7873ab10d
ScubaStve <thomas.steve091990@gmail.com>
2026-05-12 21:15:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.options('*', cors());
 app.use(express.json({ limit: '5mb' }));
 
 app.post('/send', async (req, res) => {
